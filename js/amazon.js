222185d75781640aba817b8b3a282c2251a7ecf5
Juanhoyos322 <juandiegohoyosrangel@gmail.com>
2023-11-15 14:16:00 -0500

    amazon

---
+++ js/amazon.js
@@ -0,0 +1,44 @@
+let imgtub1 = document.getElementById("imgtub1");
+let imgtub2 = document.getElementById("imgtub2");
+let imgtub3 = document.getElementById("imgtub3");
+let imgtub4 = document.getElementById("imgtub4");
+let imgtub5 = document.getElementById("imgtub5");
+let imgtub6 = document.getElementById("imgtub6");
+let imgtub7 = document.getElementById("imgtub7");
+let imgMain = document.getElementById("imgPrincipal");
+let boxmodal = document.getElementById("box-modal");
+let modal = document.getElementById("modal");
+let mainContent = document.getElementById("mainContent");
+let btnclose = document.getElementById("btnclose");
+
+window.onload = () => {
+  boxmodal.classList.remove("visible")
+}
+imgtub1.addEventListener("click",() => {
+  imgMain.setAttribute("src", "img/juan/amazon1.jpg");
+});
+imgtub2.addEventListener("click",() => {
+  imgMain.setAttribute("src", "img/juan/amazon2.jpg");
+});
+imgtub3.addEventListener("click",() => {
+  imgMain.setAttribute("src", "img/juan/amazon3.jpg");
+});
+imgtub4.addEventListener("click",() => {
+  imgMain.setAttribute("src", "img/juan/amazon4.jpg");
+});
+imgtub5.addEventListener("click",() => {
+  imgMain.setAttribute("src", "img/juan/amazon5.jpg");
+});
+imgtub6.addEventListener("click",() => {
+  imgMain.setAttribute("src", "img/juan/amazon6.jpg");
+});
+imgtub7.addEventListener("click",() => {
+    imgMain.setAttribute("src", "img/juan/amazon7.jpg");
+  });
+btnclose.addEventListener("click", function() {
+  mainContent.style.display = "none";
+});
+
+function abrir(){
+  boxmodal.classList.add("visible")
+}
